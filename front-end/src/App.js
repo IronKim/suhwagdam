bf44792dda16c308db11c4666ca0ac44a31f118b
IronKim <tankokim@gmail.com>
2024-06-11 18:53:46 +0900

    프론트엔드 초기 세팅

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {RecoilRoot} from "recoil";
+
+function App() {
+  return (
+        <RecoilRoot>
+          <View />
+        </RecoilRoot>
+  );
+}
+
+export default App;
---
+++ front-end/src/View.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter} from "react-router-dom";
+
+const View = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                {/* <Header /> */}
+
+                {/* <Footer /> */}
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default View;
