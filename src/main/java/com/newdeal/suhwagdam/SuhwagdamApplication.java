916a5bf9fc2ee5b95202e1fbe8aa1f58570d5f2b
IronKim <tankokim@gmail.com>
2024-06-11 11:47:05 +0900

    프로젝트 초기 세팅(백엔드)

---
+++ src/main/java/com/newdeal/suhwagdam/SuhwagdamApplication.java
@@ -0,0 +1,13 @@
+package com.newdeal.suhwagdam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhwagdamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuhwagdamApplication.class, args);
+	}
+
+}
