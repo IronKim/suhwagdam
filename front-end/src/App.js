c67bc2a5d721a4c57bcc17f31f86aba487c8e9bd
fantazina <137284904+fantazina@users.noreply.github.com>
2024-06-18 16:19:37 +0900

    App.js 수정

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,11 @@
 import {RecoilRoot} from "recoil";
 import View from "./View"
-import Main from "./pages/Main";
 import './css/default.css';
 
 function App() {
   return (
         <RecoilRoot>
           <View />
-          <Main />
         </RecoilRoot>
   );
 }
