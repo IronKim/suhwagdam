a886c55cb6674b25521ee33f2bbe7ff27b6d4182
Sun Kyoung <tjsrud18@gmail.com>
2024-06-27 10:27:45 +0900

    회원가입 완료

--- front-end/src/api/AuthApiService.js
+++ front-end/src/api/AuthApiService.js
@@ -3,3 +3,12 @@ import {apiClient} from "./ApiClient";
 export const login = (loginRequest) => {
     return apiClient.post('api/v1/auth/login', loginRequest);
   };
+export const checkId = (accountId) => {
+    return apiClient.get(`api/v1/auth/id-check/${accountId}`);
+  };
+export const checkEmail = (email) => {
+    return apiClient.get(`api/v1/auth/email-check/${email}`);
+  };
+export const join = (usertDTO) => {
+    return apiClient.post('api/v1/auth/join', usertDTO);
+  };
--- front-end/src/components/Butt.jsx
+++ front-end/src/components/Butt.jsx
@@ -7,7 +7,9 @@ const StyledButton = styled.button`
   height:${(props) => props.height || "50px"};
   padding: 6px 12px;
   border-radius: 8px;
-  font-size: 20px;
+  font-size: ${(props) => props.height || "20px"};
+  padding-top: 10px;
+  padding-bottom: 10px;
   line-height: 1.5;
   border: 1px solid lightgray;
   font-weight: bold;
